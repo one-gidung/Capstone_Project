15e63aba3cbe80ee2b0bdcc400add76108b1c06c
HyeongSuk, Oh <hyngsk.o@gmail.com>
2021-04-11 04:56:25 +0900

    commit

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from flask import render_template
 from upbit import Upbit
 
 app = Flask(__name__)
-# upbit = Upbit()
-# upbit.get_hour_candles('KRW-BTC')
+upbit = Upbit()
+upbit.get_hour_candles('KRW-BTC')
 
 token = '1787156675:AAE6V94s-0ov58WebD4mzhsgjSkms4a0jps'
 api_url = 'https://api.telegram.org'
@@ -27,38 +27,39 @@ api_url = 'https://api.telegram.org'
 
 @app.route(f'/{token}', methods=['POST'])
 def telegram():
-    # print(request.get_json())
-    chat_id = request.json['message']['from']['id']
-    text = request.json['message']['text']
+    print(request.get_json())
+    chat_id = request.get_json().get('message').get('from').get('id')
+    text = request.get_json()['message']['text']
     result = 'fail'
     if text == '/code':
         result = 'success'
-    requests.get(f'{api_url}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
+
+    requests.get(f'{api_url}/bot{token}/sendMessage?chat_id={chat_id}&text={text}+{result}')
+
     return '', 200
 
+@app.route('/')
+def root():
+    market = request.args.get('market')
+    if market is None or market == '':
+        return 'No market parameter'
 
-# @app.route('/')
-# def root():
-#     market = request.args.get('market')
-#     if market is None or market == '':
-#         return 'No market parameter'
-#
-#     candles = upbit.get_hour_candles(market)
-#     if candles is None:
-#         return 'invalid market: {}'.format(market)
-#
-#     label = market
-#     xlabels = []
-#     dataset = []
-#     i = 0
-#     for candle in candles:
-#         xlabels.append('')
-#         dataset.append(candle['trade_price'])
-#         i += 1
-#     return render_template('chart.html', **locals())
+    candles = upbit.get_hour_candles(market)
+    if candles is None:
+        return 'invalid market: {}'.format(market)
+
+    label = market
+    xlabels = []
+    dataset = []
+    i = 0
+    for candle in candles:
+        xlabels.append('')
+        dataset.append(candle['trade_price'])
+        i += 1
+    return render_template('chart.html', **locals())
 
 
 
 if __name__ == '__main__':
-    app.debug = False
-    app.run()
+    app.debug = True
+    app.run(host='0.0.0.0', threaded=False)
