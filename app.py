308b5eeae8cced9eb291fdfd716fa825f4c51fa9
HyeongSuk, Oh <hyngsk.o@gmail.com>
2021-04-11 04:02:51 +0900

    commit

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def telegram():
     result = 'fail'
     if text == '/code':
         result = 'success'
-    requests.get(f'{api_url}/bot{token}/sendMessage?chat_id={chat_id}&text={result}+{text}')
+    requests.get(f'{api_url}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
     return '',200
 
 @app.route('/')
