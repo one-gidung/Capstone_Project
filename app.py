f45c298d6e53b2891813b0a4e48504af2882acfb
HyeongSuk, Oh <hyngsk.o@gmail.com>
2021-04-11 05:57:18 +0900

    commit

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def telegram():
     print(request.get_json())
     chat_id = request.get_json().get('message').get('from').get('id')
     text = request.get_json()['message']['text'].split()
-    mtype = request.get_json()['message']['entities']['type']
+    mtype = request.get_json()['message']['entities'][0]['type']
 
     print('\n', text)
     result = ''
