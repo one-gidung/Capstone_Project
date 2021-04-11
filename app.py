cb7fe024266611106ff54547e4a08891525c04e7
HyeongSuk, Oh <hyngsk.o@gmail.com>
2021-04-11 18:16:03 +0900

    commit

--- app.py
+++ app.py
@@ -31,10 +31,13 @@ api_url = 'https://api.telegram.org'
 def telegram_response():
     print(request.get_json())
     print()
-    chat_id = request.get_json().get('message').get('from').get('id')
-    text = request.get_json()['message']['text'].split()
-    date = request.get_json()['message']['date']
-    if text[0][0] == '/': # or text[0][1:] not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz':
+    try:
+        chat_id = request.get_json().get('message').get('from').get('id')
+        text = request.get_json()['message']['text'].split()
+        date = request.get_json()['message']['date']
+    except:
+        pass
+    if text[0][0] == '/':  # or text[0][1:] not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz':
         # entities = request.get_json().get('message').get('entities')
         # print(f'length : {entities[0]["length"]}\ntype : {entities[0]["type"]}')
         if text[0] == '/start':
